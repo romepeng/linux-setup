f73a90fd4f6113a27131ecf7ad95f78f73e3b9ec
romepeng <romepeng@outlook.com>
2022-04-12 15:38:30 +0800

    Update Install_miniconda.sh

--- pyenv/Install_miniconda.sh
+++ pyenv/Install_miniconda.sh
@@ -4,7 +4,7 @@
 clear
 
 echo -e "\t start installsilent miniconda"
-MINICONDA_PATH="$HOME/minconda"
+MINICONDA_PATH="$HOME/miniconda"
 wget  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(uname -m).sh -O  ~/miniconda.sh bash  ~/miniconda.sh -b -p ${MINICONDA_PATH}
-#${MINICONDA_PATH}/bin/conda init
-#source  ~/.bashrc
+${MINICONDA_PATH}/bin/conda init
+source  ~/.bashrc
