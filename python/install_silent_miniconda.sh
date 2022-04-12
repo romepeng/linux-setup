dc14e2b605755f128ba7e0e3c2dcefe20d73f59a
romepeng <romepeng@outlook.com>
2022-04-12 15:00:12 +0800

    Update install_silent_miniconda.sh

--- python/install_silent_miniconda.sh
+++ python/install_silent_miniconda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+# for ubuntu x_86_64 or aarch_64 cpu
 #from <https://conda.io/projects/conda/en/latest/user-guide/install/macos.html#install-macos-silent> 
+clear
 echo -e "\t install miniconda"
 MINICONDA_PATH="$HOME/minconda"
 wget  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(uname -m).sh -O  ~/miniconda.sh \
