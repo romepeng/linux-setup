a4597326590e82ce8e9362133ecf383e7d9ebd6d
romepeng <romepeng@outlook.com>
2022-04-20 20:20:46 +0800

    Update install_ssl_cert_crontab.sh

--- ssl/install_ssl_cert_crontab.sh
+++ ssl/install_ssl_cert_crontab.sh
@@ -5,15 +5,15 @@ apt install -y socat
 
 curl https://get.acme.sh | sh -s email=romepeng@outlook.com
 
-echo "alias acme.sh='~/.acme.sh/acme.sh'" >> ~/.bashrc
-source ~/.bashrc
+#echo "alias acme.sh='~/.acme.sh/acme.sh'" >> ~/.bashrc
+#source ~/.bashrc
 
 #read -p "input email for ssl: " EMAIL
 #echo -e "your email : $EMAIL"
 
-#acme.sh --register-account -m $EMAIL
+#~/.acme.sh/acme.sh --register-account -m $EMAIL
 
 read -p "input domain for ssl: " DOMAIN
 echo -e "your domain is $DOMAIN"
 
-acme.sh --installcert -d $DOMAIN --key-file /root/private.key --fullchain-file /root/cert.crt
+~/.acme.sh/acme.sh --installcert -d $DOMAIN --key-file /root/private.key --fullchain-file /root/cert.crt
