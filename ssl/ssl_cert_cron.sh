982e6f98c460ebb67f441a21a76d9fd634adaa29
romepeng <romepeng@outlook.com>
2022-04-20 20:35:24 +0800

    Update ssl_cert_cron.sh

--- ssl/ssl_cert_cron.sh
+++ ssl/ssl_cert_cron.sh
@@ -2,7 +2,9 @@
 
 crontab -l > /tmp/crontab-backup
 
-echo "0 1 * * *   bash /root/ssl-cert-renew.sh"  >> /tmp/crontab-backup
+#echo "0 1 * * *   bash /root/ssl-cert-renew.sh"  >> /tmp/crontab-backup
+DOMAIN="rbobosz.tk"
+echo "0 1 * * *  ~/.acme.sh/acme.sh --installcert -d $DOMAIN --key-file /root/ssl/private.key --fullchain-file /root/ssl/cert.crt
 
 crontab -u $USER /tmp/crontab-backup
 
