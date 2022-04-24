e6e150cf1e0006108ef8f4ba0fc22dda4b54d188
romepeng <romepeng@outlook.com>
2022-04-24 08:07:59 +0800

    Update zsh/install_omz_zsh.sh

--- zsh/install_omz_zsh.sh
+++ zsh/install_omz_zsh.sh
@@ -3,5 +3,5 @@
 #https://www.tecmint.com/install-oh-my-zsh-in-ubuntu/
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#  sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+# sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
