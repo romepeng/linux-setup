069fbd333c1f39c3445c8be9bdba9388423da102
romepeng <romepeng@outlook.com>
2022-05-03 12:05:34 +0800

    Create xcaddy-install.sh

---
+++ caddy/xcaddy-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-xcaddy.asc
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-xcaddy.list
+sudo apt update
+sudo apt install xcaddy
