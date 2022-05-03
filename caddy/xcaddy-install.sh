ab73a996b49a992ff114e3bc5a29ed7895229b30
romepeng <romepeng@outlook.com>
2022-05-03 18:57:30 +0800

    Update xcaddy-install.sh

--- caddy/xcaddy-install.sh
+++ caddy/xcaddy-install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-xcaddy.asc
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-xcaddy.list
 sudo apt update
 sudo apt install xcaddy
+
+
+#the xcaddy command has two primary uses:
+#1.Compile custom caddy binaries
+#2.A replacement for go run while developing Caddy plugins
