765bd01766477850090b4d99fc3fbc59cbef6cc3
romepeng <romepeng@outlook.com>
2022-10-10 12:15:44 +0800

    Update setup_first_arm64.sh

--- setup_first_arm64.sh
+++ setup_first_arm64.sh
@@ -157,85 +157,86 @@ install_miniconda(){
 	wget https://raw.githubusercontent.com/romepeng/linux-onekey-setup/main/pyenv/Install_miniconda.sh  && bash Install_miniconda.sh && conda update -y conda
 }
 
-Install_ssl_onekey_cfapi(){
-    clear
-    
+install_ssl_onekey_cfapi() {
+    echo -E ""
     # check root
     [[ $EUID -ne 0 ]] && LOGE "错误:  必须使用root用户运行此脚本!\n" && exit 1
     
-    echo-E ""
     LOGD "******使用说明******"
     LOGI "该脚本将使用Acme脚本申请证书,使用时需保证:"
     LOGI "1.知晓Cloudflare 注册邮箱"
     LOGI "2.知晓Cloudflare Global API Key"
     LOGI "3.域名已通过Cloudflare进行解析到当前服务器"
     LOGI "4.该脚本申请证书默认安装路径为/root/cert目录"
-    confirm "我已确认以上内容[y/n]""y"
-    if[ $? -eq 0 ]; then
-        cd~
+    
+    confirm "我已确认以上内容[y/n]" "y"
+    
+    if [ $? -eq 0 ]; then
+        cd ~
         LOGI "安装Acme脚本"
-        curl https://get.acme.sh |sh
-        if[ $? -ne 0 ]; then
+        curl https://get.acme.sh | sh
+        if [ $? -ne 0 ]; then
             LOGE "安装acme脚本失败"
-            exit1
+            exit 1
         fi
         CF_Domain=""
         CF_GlobalKey=""
         CF_AccountEmail=""
         certPath=/root/cert
-        if[ !-d"$certPath"]; then
+        if [ ! -d "$certPath" ]; then
             mkdir $certPath
         else
             rm -rf $certPath
             mkdir $certPath
         fi
         LOGD "请设置域名:"
-        read-p "Input your domain here:"CF_Domain
+        read -p "Input your domain here:" CF_Domain
         LOGD "你的域名设置为:${CF_Domain}"
         LOGD "请设置API密钥:"
-        read-p "Input your key here:"CF_GlobalKey
+        read -p "Input your key here:" CF_GlobalKey
         LOGD "你的API密钥为:${CF_GlobalKey}"
         LOGD "请设置注册邮箱:"
-        read-p "Input your email here:"CF_AccountEmail
+        read -p "Input your email here:" CF_AccountEmail
         LOGD "你的注册邮箱为:${CF_AccountEmail}"
         ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-        if[ $? -ne 0 ]; then
+        if [ $? -ne 0 ]; then
             LOGE "修改默认CA为Lets'Encrypt失败,脚本退出"
-            exit1
+            exit 1
         fi
-        exportCF_Key="${CF_GlobalKey}"
-        exportCF_Email=${CF_AccountEmail}
-        ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain}-d *.${CF_Domain}--log
-        if[ $? -ne 0 ]; then
+        export CF_Key="${CF_GlobalKey}"
+        export CF_Email=${CF_AccountEmail}
+        ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain} -d *.${CF_Domain} --log
+        if [ $? -ne 0 ]; then
             LOGE "证书签发失败,脚本退出"
-            exit1
+            exit 1
         else
             LOGI "证书签发成功,安装中..."
         fi
-        ~/.acme.sh/acme.sh --installcert -d ${CF_Domain}-d *.${CF_Domain}--ca-file /root/cert/ca.cer \
+        ~/.acme.sh/acme.sh --installcert -d ${CF_Domain} -d *.${CF_Domain} --ca-file /root/cert/ca.cer \
         --cert-file /root/cert/${CF_Domain}.cer --key-file /root/cert/${CF_Domain}.key \
         --fullchain-file /root/cert/fullchain.cer
-        if[ $?-ne0 ];then
+        if [ $? -ne 0 ]; then
             LOGE "证书安装失败,脚本退出"
-            exit1
+            exit 1
         else
             LOGI "证书安装成功,开启自动更新..."
         fi
         ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-        if[ $? -ne 0 ]; then
+        if [ $? -ne 0 ]; then
             LOGE "自动更新设置失败,脚本退出"
             ls -lah cert
             chmod 755 $certPath
-            exit1
+            exit 1
         else
             LOGI "证书已安装且已开启自动更新,具体信息如下"
             ls -lah cert
             chmod 755 $certPath
         fi
     else
-        menu
+    	menu
     fi
 }
+
 
 
 function menu(){
