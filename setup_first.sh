2e5876696c891bcf6d38934204f565e55a5d5746
romepeng <15272552+romepeng@users.noreply.github.com>
2022-12-15 15:47:45 +0800

    Update setup_first.sh

--- setup_first.sh
+++ setup_first.sh
@@ -167,11 +167,11 @@ install_ssl_cert_issue() {
     LOGI "3.域名已通过Cloudflare进行解析到当前服务器"
     LOGI "4.该脚本申请证书默认安装路径为/root/cert目录"
     confirm "我已确认以上内容[y/n]""y"
-    if[ $?-eq0 ];then
+    if [ $?-eq0 ]; then
         cd~
         LOGI "安装Acme脚本"
         curl https://get.acme.sh |sh
-        if[ $?-ne0 ];then
+        if [ $?-ne0 ]; then
             LOGE "安装acme脚本失败"
             exit1
         fi
@@ -179,7 +179,7 @@ install_ssl_cert_issue() {
         CF_GlobalKey=""
         CF_AccountEmail=""
         certPath=/root/cert
-        if[ !-d"$certPath"];then
+        if [ !-d"$certPath"]; then
             mkdir $certPath
         else
             rm -rf $certPath
@@ -195,14 +195,14 @@ install_ssl_cert_issue() {
         read-p "Input your email here:"CF_AccountEmail
         LOGD "你的注册邮箱为:${CF_AccountEmail}"
         ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-        if[ $?-ne0 ];then
+        if [ $?-ne0 ]; then
             LOGE "修改默认CA为Lets'Encrypt失败,脚本退出"
             exit1
         fi
         exportCF_Key="${CF_GlobalKey}"
         exportCF_Email=${CF_AccountEmail}
         ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${CF_Domain}-d *.${CF_Domain}--log
-        if[ $?-ne0 ];then
+        if [ $?-ne0 ]; then
             LOGE "证书签发失败,脚本退出"
             exit1
         else
@@ -211,14 +211,14 @@ install_ssl_cert_issue() {
         ~/.acme.sh/acme.sh --installcert -d ${CF_Domain}-d *.${CF_Domain}--ca-file /root/cert/ca.cer \
         --cert-file /root/cert/${CF_Domain}.cer --key-file /root/cert/${CF_Domain}.key \
         --fullchain-file /root/cert/fullchain.cer
-        if[ $?-ne0 ];then
+        if [ $?-ne0 ]; then
             LOGE "证书安装失败,脚本退出"
             exit1
         else
             LOGI "证书安装成功,开启自动更新..."
         fi
         ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-        if[ $?-ne0 ];then
+        if [ $?-ne0 ]; then
             LOGE "自动更新设置失败,脚本退出"
             ls -lah cert
             chmod 755 $certPath
