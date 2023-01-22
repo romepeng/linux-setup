659e06dd99b4658032df4fbfa711bc13f60a0eea
romepeng <15272552+romepeng@users.noreply.github.com>
2023-01-22 19:57:42 +0800

    Update setup_frist.sh

--- setup/setup_frist.sh
+++ setup/setup_frist.sh
@@ -89,7 +89,28 @@ function git_config(){
         echo
 
 }
+# debain-base miniconda install
+function install_miniconda() {
+        sudo -i
+        # Install our public GPG key to trusted store
+        curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg
+        install -o root -g root -m 644 conda.gpg /usr/share/keyrings/conda-archive-keyring.gpg
 
+        # Check whether fingerprint is correct (will output an error message otherwise)
+        gpg --keyring /usr/share/keyrings/conda-archive-keyring.gpg --no-default-keyring --fingerprint 34161F5BF5EB1D4BFBBB8F0A8AEB4F8B29D82806
+
+        # Add our Debian repo
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" > /etc/apt/sources.list.d/conda.list
+
+        **NB:** If you receive a Permission denied error when trying to run the above command (because `/etc/apt/sources.list.d/conda.list` is write protected), try using the following command instead:
+        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/conda-archive-keyring.gpg] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | sudo tee -a /etc/apt/sources.list.d/conda.list
+
+        apt update 
+        apt install conda
+
+        source /opt/conda/etc/profile.d/conda.sh
+        conda -V
+}
 
 function menu(){
         clear
@@ -105,6 +126,7 @@ function menu(){
         echo -e "\t4. Python3 dauflt "
         echo -e "\t5. Install docker "
         echo -e "\t6. Install tool app "
+        echo -e "\t7. Install miniconda "
         echo -e "\t0. Exit menu \n\n "
         echo -en "\t\tEnter an option: "
         read -n 1 option
